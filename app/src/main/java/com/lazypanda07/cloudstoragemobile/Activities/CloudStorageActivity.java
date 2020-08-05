f9c0cfc2653670da45a9e1a90d06aeb6175e2332
Semen Gricenko <semengricenko@gmail.com>
2020-08-05 16:44:11 +0500

    Added popup menus for list view items and layout

--- app/src/main/java/com/lazypanda07/cloudstoragemobile/Activities/CloudStorageActivity.java
+++ app/src/main/java/com/lazypanda07/cloudstoragemobile/Activities/CloudStorageActivity.java
@@ -5,9 +5,11 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,7 +44,7 @@ public class CloudStorageActivity extends AppCompatActivity
 		fileData = new ArrayList<>();
 		adapter = new PortraitCloudStorageListViewAdapter(getApplicationContext(), fileData);
 
-		ListView filesList = findViewById(R.id.files_list);
+		final ListView filesList = findViewById(R.id.files_list);
 		Intent intent = getIntent();
 
 		login = intent.getStringExtra("login");
@@ -52,35 +54,114 @@ public class CloudStorageActivity extends AppCompatActivity
 
 		NetworkFunctions.getFiles(ref, fileData, adapter, login, password);
 
-		//TODO: refactor
-		filesList.setOnItemClickListener(new AdapterView.OnItemClickListener()
-		{
-			@Override
-			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-			{
-				String fileName = ((TextView) view.findViewById(R.id.name)).getText().toString();
-				String sFileSize = ((TextView) view.findViewById(R.id.size)).getText().toString();
-				long fileSize = Long.parseLong(sFileSize.substring(0, sFileSize.indexOf(' ')));
-
-				NetworkFunctions.downloadFile(ref, fileName, fileSize, login, password);
-			}
-		});
-
-		//TODO: refactor
 		filesList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
 		{
 			@Override
 			public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l)
 			{
-				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-				intent.setType("*/*");
-				intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-				startActivityForResult(Intent.createChooser(intent, getApplicationContext().getResources().getString(R.string.choose_file)), Constants.GET_FILE);
+				showOnItemLongClickPopupMenu(view);
 
 				return true;
 			}
 		});
+
+		findViewById(R.id.cloud_storage).setOnLongClickListener(new View.OnLongClickListener()
+		{
+			@Override
+			public boolean onLongClick(View view)
+			{
+				showOnLayoutLongClickPopupMenu(filesList);
+
+				return true;
+			}
+		});
+
+		filesList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+			{
+				//TODO: open file or directory
+			}
+		});
+	}
+
+	private void showOnLayoutLongClickPopupMenu(final View view)
+	{
+		PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+		popupMenu.inflate(R.menu.on_layout_long_click_popup_menu);
+
+		popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+		{
+			@Override
+			public boolean onMenuItemClick(MenuItem menuItem)
+			{
+				switch (menuItem.getItemId())
+				{
+					case R.id.upload_file:
+						Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+						intent.setType("*/*");
+						intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+						startActivityForResult(Intent.createChooser(intent, getApplicationContext().getResources().getString(R.string.choose_file)), Constants.GET_FILE);
+
+						break;
+
+					case R.id.create_folder:
+						//TODO: create folder method
+
+						break;
+				}
+
+				return false;
+			}
+		});
+
+		popupMenu.show();
+	}
+
+	private void showOnItemLongClickPopupMenu(final View view)
+	{
+		PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+		popupMenu.inflate(R.menu.on_item_long_click_popup_menu);
+
+		popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+		{
+			@Override
+			public boolean onMenuItemClick(MenuItem menuItem)
+			{
+				String fileName = ((TextView) view.findViewById(R.id.name)).getText().toString();
+
+				switch (menuItem.getItemId())
+				{
+					case R.id.download_file:
+						String sFileSize = ((TextView) view.findViewById(R.id.size)).getText().toString();
+						long fileSize = Long.parseLong(sFileSize.substring(0, sFileSize.indexOf(' ')));
+
+						NetworkFunctions.downloadFile(ref, fileName, fileSize, login, password);
+
+						break;
+
+					case R.id.remove_file:
+						NetworkFunctions.removeFile(ref, fileName, login, password, fileData, adapter);
+
+						break;
+
+					case R.id.upload_file:
+						Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+						intent.setType("*/*");
+						intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+						startActivityForResult(Intent.createChooser(intent, getApplicationContext().getResources().getString(R.string.choose_file)), Constants.GET_FILE);
+
+						break;
+				}
+
+				return false;
+			}
+		});
+
+		popupMenu.show();
 	}
 
 	@Override
