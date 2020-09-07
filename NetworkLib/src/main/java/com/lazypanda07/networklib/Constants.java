f9a1d1acf70073b3ea8707ebe611e85474231401
Semen Gricenko <semengricenko@gmail.com>
2020-09-08 03:13:42 +0500

    Fix http headers name, update servers

--- NetworkLib/src/main/java/com/lazypanda07/networklib/Constants.java
+++ NetworkLib/src/main/java/com/lazypanda07/networklib/Constants.java
@@ -18,40 +18,40 @@ public class Constants
 
 	public static class RequestType
 	{
-		public static final String ACCOUNT_TYPE = "Account request";
-		public static final String FILES_TYPE = "Files request";
-		public static final String EXIT_TYPE = "Exit request";
-		public static final String CANCEL_TYPE = "Cancel request";
-		public static final String CONTROL_TYPE = "Control request";
+		public static final String ACCOUNT_TYPE = "Account-Request";
+		public static final String FILES_TYPE = "Files-Request";
+		public static final String EXIT_TYPE = "Exit-Request";
+		public static final String CANCEL_TYPE = "Cancel-Request";
+		public static final String CONTROL_TYPE = "Control-Request";
 	}
 
 	public static class NetworkRequests
 	{
 		public static final String EXIT = "EOSS";
-		public static final String CANCEL_OPERATION = "Cancel operation";
+		public static final String CANCEL_OPERATION = "Cancel-Operation";
 	}
 
 	public static class ControlRequests
 	{
-		public static final String NEXT_FOLDER = "Next folder";
-		public static final String PREVIOUS_FOLDER = "Previous folder";
-		public static final String SET_PATH = "Set path";
+		public static final String NEXT_FOLDER = "Next-Folder";
+		public static final String PREVIOUS_FOLDER = "Previous-Folder";
+		public static final String SET_PATH = "Set-Path";
 	}
 
 	public static class AccountRequests
 	{
 		public static final String AUTHORIZATION = "Authorization";
 		public static final String REGISTRATION = "Registration";
-		public static final String SET_LOGIN = "Set login";
+		public static final String SET_LOGIN = "Set-Login";
 	}
 
 	public static class FilesRequests
 	{
-		public static final String UPLOAD_FILE = "Upload file";
-		public static final String DOWNLOAD_FILE = "Download file";
-		public static final String SHOW_ALL_FILES_IN_DIRECTORY = "Show all files in directory";
-		public static final String REMOVE_FILE = "Remove file";
-		public static final String CREATE_FOLDER = "Create folder";
+		public static final String UPLOAD_FILE = "Upload-File";
+		public static final String DOWNLOAD_FILE = "Download-File";
+		public static final String SHOW_ALL_FILES_IN_DIRECTORY = "Show-All-Files-In-Folder";
+		public static final String REMOVE_FILE = "Remove-File";
+		public static final String CREATE_FOLDER = "Create-Folder";
 	}
 
 	public static class Responses
